566cb0f7b11687e3ea01d9610ffc2f252306c8bf
ssrome <ssamuelrome@gmail.com>
2019-01-07 00:01:36 +0000

    Updated README and scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,35 @@
-let onclick = () => {
-    return ;
-}
+$(document).ready(function() {
+    let quote;
+    let author;
+    let link;
+
+    function getNewQuote() {
+        $.ajax({
+            url: 'http://api.forismatic.com/api/1.0/',
+            jsonp: 'jsonp',
+            dataType: 'jsonp',
+            data: {
+                method: 'getQuote',
+                lang: 'en',
+                format: 'jsonp'
+            },
+            success: function(response){
+                quote = response.quoteText;
+                author = response.quoteAuthor;
+                console.log(response.quoteAuthor);
+                $('#text').text(quote);
+                if (author){
+                    $('#author').text ("by " + author)
+                } else {
+                    $('#author').text ("by unknown");
+                }
+            }
+            
+        });
+    }
+    getNewQuote();
+
+    $('#new-quote').on('click', function() {
+        getNewQuote();
+    });
+})
