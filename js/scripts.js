825881ae517e19ad3823d37c726b58f92b66a070
ssrome <ssamuelrome@gmail.com>
2019-01-07 23:36:12 +0000

    Updated API URL to https

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
 
     function getNewQuote() {
         $.ajax({
-            url: 'http://api.forismatic.com/api/1.0/',
+            url: 'https://api.forismatic.com/api/1.0/',
             jsonp: 'jsonp',
             dataType: 'jsonp',
             data: {
